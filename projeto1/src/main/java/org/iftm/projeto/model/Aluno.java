42a6b3031de621ce8bbfaa0b8952521aa9e31f8c
shirley cristina mi <69362121+shirleycristinami@users.noreply.github.com>
2022-09-29 20:57:38 -0300

    atualizado

--- projeto1/src/main/java/org/iftm/projeto/model/Aluno.java
+++ projeto1/src/main/java/org/iftm/projeto/model/Aluno.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+ 
 @Entity
 public class Aluno implements Serializable{	
 	private static final long serialVersionUID = 1L;

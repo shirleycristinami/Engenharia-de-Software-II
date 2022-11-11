da66f80e11cc61ad89703de6aa46229942a1c723
shirley cristina mi <69362121+shirleycristinami@users.noreply.github.com>
2022-11-11 20:27:03 -0300

    Atualização do sistema com interface usuário

--- projeto1/src/main/java/org/iftm/projeto/view/TelaEmpresa.java
+++ projeto1/src/main/java/org/iftm/projeto/view/TelaEmpresa.java
@@ -3,8 +3,13 @@ package org.iftm.projeto.view;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class TelaEmpresa extends JInternalFrame {
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -26,8 +31,26 @@ public class TelaEmpresa extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public TelaEmpresa() {
+		getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 12));
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nome: ");
+		lblNewLabel.setBounds(10, 33, 46, 14);
+		getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(48, 30, 206, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("INSERIR");
+		btnNewButton.setBounds(288, 29, 89, 23);
+		getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(168, 11, 46, 14);
+		getContentPane().add(lblNewLabel_1);
 		setBounds(100, 100, 450, 300);
 
 	}
-
 }
--- projeto1/src/main/java/org/iftm/projeto/view/TelaPrincipal.java
+++ projeto1/src/main/java/org/iftm/projeto/view/TelaPrincipal.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JDesktopPane;
 import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
 
 public class TelaPrincipal {
 
@@ -43,6 +49,36 @@ public class TelaPrincipal {
 		
 		JDesktopPane desktop = new JDesktopPane();
 		frame.getContentPane().add(desktop, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("CADASTRO");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("New menu item");
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenu mnNewMenu_1 = new JMenu("CONSULTA");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem_3);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("New menu item");
+		mnNewMenu_1.add(mntmNewMenuItem_4);
+		
+		JMenu mnNewMenu_2 = new JMenu("AJUDA");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Sobre");
+		mntmNewMenuItem_5.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		mnNewMenu_2.add(mntmNewMenuItem_5);
 	}
-
 }
